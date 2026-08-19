272efcb1cbfc144060c6c891401124932368a5c9
FletcherFX <temagen786@gmail.com>
2026-08-20 00:53:49 +0500

    Create links.js

---
+++ links.js
@@ -0,0 +1,19 @@
+const links = {
+    officialSite: "https://fletcherfx.github.io/natrium/",
+    minecraftInside: "https://minecraft-inside.ru/modpacks/194696-natrium.html",
+    githubRepo: "https://github.com/FletcherFX/natrium",
+    telegramChat: "https://t.me/NatriumChat",
+    telegramChannel: "https://t.me/NatriumProject",
+    bugReport: "https://www.donationalerts.com/r/fletcherfx",
+    donationAlerts: "https://www.donationalerts.com/r/fletcherfx",
+    modrinth: ""
+};
+
+document.getElementById("site-link").href = links.officialSite;
+document.getElementById("mineinside-link").href = links.minecraftInside;
+document.getElementById("github-link").href = links.githubRepo;
+document.getElementById("tgchat-link").href = links.telegramChat;
+document.getElementById("tgchannel-link").href = links.telegramChannel;
+document.getElementById("bug-link").href = links.bugReport;
+document.getElementById("da-link").href = links.donationAlerts;
+document.getElementById("modrinth-link").href = links.modrinth;
